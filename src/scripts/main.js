19f350f3d11a9bb8663c8118ea88a2d0e66fe231
James Bunt <james.bunt@vendhq.com>
2014-10-04 14:04:18 +1300

    Install voltaire theme.

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,7 +2,7 @@
 
 // Require Node modules in the browser thanks to Browserify: http://browserify.org
 var bespoke  = require('bespoke'),
-    cube     = require('bespoke-theme-cube'),
+    voltaire = require('bespoke-theme-voltaire'),
     keys     = require('bespoke-keys'),
     touch    = require('bespoke-touch'),
     scale    = require('bespoke-scale'),
@@ -12,7 +12,7 @@ var bespoke  = require('bespoke'),
 
 // Bespoke.js
 bespoke.from('article', [
-    cube(),
+    voltaire(),
     keys(),
     touch(),
     scale(),
