6c7e8fb499886c0ff920f24e144fc169280ea809
James Bunt <james.bunt@vendhq.com>
2014-10-04 14:02:16 +1300

    Update indentation to 4 spaces to match editorconfig.

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,26 +1,27 @@
+/*global require*/
+
 // Require Node modules in the browser thanks to Browserify: http://browserify.org
-var bespoke = require('bespoke'),
-  cube = require('bespoke-theme-cube'),
-  keys = require('bespoke-keys'),
-  touch = require('bespoke-touch'),
-  scale = require('bespoke-scale'),
-  hash = require('bespoke-hash'),
-  progress = require('bespoke-progress'),
-  state = require('bespoke-state');
+var bespoke  = require('bespoke'),
+    cube     = require('bespoke-theme-cube'),
+    keys     = require('bespoke-keys'),
+    touch    = require('bespoke-touch'),
+    scale    = require('bespoke-scale'),
+    hash     = require('bespoke-hash'),
+    progress = require('bespoke-progress'),
+    state    = require('bespoke-state');
 
 // Bespoke.js
 bespoke.from('article', [
-  cube(),
-  keys(),
-  touch(),
-  scale(),
-  hash(),
-  progress(),
-  state()
+    cube(),
+    keys(),
+    touch(),
+    scale(),
+    hash(),
+    progress(),
+    state()
 ]);
 
 // Prism syntax highlighting
 // This is actually loaded from "bower_components" thanks to
 // debowerify: https://github.com/eugeneware/debowerify
 require('prism');
-
